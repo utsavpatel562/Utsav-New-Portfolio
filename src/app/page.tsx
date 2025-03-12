ec2bcc4f541ae7542c92a91622552dc04f51c97f
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-12 13:40:20 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h2 className="bg-white text-slate-900 h-screen text-3xl text-center p-10">
+        New Portfolio Comming Soon!
+      </h2>
+    </>
+  );
+}

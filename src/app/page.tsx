ce078127bb46d84736cc1102eb80a6797d3ffe1a
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-13 21:41:56 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
       <h2 className="bg-white text-slate-900 h-screen text-3xl text-center p-10">
-        New Portfolio Comming Soon!
+        <Button variant={"outline"} className="text-slate-100 cursor-pointer">
+          Utsav Portfolio
+        </Button>
       </h2>
     </>
   );

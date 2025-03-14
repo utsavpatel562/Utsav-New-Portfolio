65c481a57143994114dfd317753c190eb1385295
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-14 12:01:47 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <h2 className="bg-white text-slate-900 h-screen text-3xl text-center p-10">
-        <Button variant={"outline"} className="text-slate-100 cursor-pointer">
+        <Button className="text-slate-100 cursor-pointer">
           Utsav Portfolio
         </Button>
       </h2>

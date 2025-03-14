4670395961a8b7a5bcec7865be677b5ac6247919
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-03-14 12:14:46 -0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/ui/Header";
 
 export default function Home() {
   return (
     <>
-      <h2 className="bg-white text-slate-900 h-screen text-3xl text-center p-10">
-        <Button className="text-slate-100 cursor-pointer">
-          Utsav Portfolio
-        </Button>
-      </h2>
+      <Header />
     </>
   );
 }
